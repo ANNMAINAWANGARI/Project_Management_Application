d27b6fa96af56e4a98183f41c5719c6e30d5bde7
ANNMAINAWANGARI <wangariann371@gmail.com>
2022-09-01 10:50:27 +0300

    login-component UI done

--- frontend/src/app/modules/admin/admin.module.ts
+++ frontend/src/app/modules/admin/admin.module.ts
@@ -4,10 +4,12 @@ import { HeaderComponent } from 'src/app/components/header/header.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { AppModule } from 'src/app/app.module';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
     AdminComponent,
+    AdminPageComponent,
   ],
   imports: [
     CommonModule,
